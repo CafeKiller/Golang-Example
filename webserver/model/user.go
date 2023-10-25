02e15e8b64bc61fc0d0e6bedfdb83770faeca01d
CoffeeKiller <18320210361@163.com>
2023-10-25 21:36:48 +0800

    [feat] update 2023_10_25 21:36:43

--- webserver/model/user.go
+++ webserver/model/user.go
@@ -1,1 +1,85 @@
 package main
+
+import (
+	"errors"
+	"github.com/labstack/echo"
+)
+
+// commandType 命令行参数类型
+type commandType int
+
+// FindOption 查询参数选项
+type FindOption int
+
+// ID 唯一识别信息
+type ID string
+
+// StringMD5 通过MD5哈希化后的字符串
+type StringMD5 string
+
+// Role 用户权限
+type Role string
+
+// response 命令执行结果
+type response struct {
+	result []interface{}
+	err    error
+}
+
+const (
+	commandFindAll commandType = iota
+	commandFindByID
+	commandFindByUserID
+)
+
+var e *echo.Echo
+
+var (
+	ErrorNotFound        = errors.New("Not found")
+	ErrorMultipleResults = errors.New("Multiple results")
+	ErrorInvalidCommand  = errors.New("Invalid Command")
+	ErrorBadParameter    = errors.New("Bad Parameter")
+	ErrorNotImplemented  = errors.New("Not Implemented")
+	ErrorOther           = errors.New("Other")
+)
+
+// 执行命令的参数
+type command struct {
+	cmdType    commandType
+	req        []interface{}
+	responseCh chan response
+}
+
+// UserDataAccessor 用户数据操作对象
+type UserDataAccessor struct {
+	stopCh    chan struct{}
+	commandCh chan command
+}
+
+type User struct {
+	ID       ID        `json:"id"`
+	UserID   string    `json:"user_id"`
+	Password StringMD5 `json:"password"`
+	FullName string    `json:"full_name"`
+	Roles    []Role    `json:"roles"`
+}
+
+// FindByUserID 通过UserID查询对应数据
+func (a *UserDataAccessor) FindByUserID(reqUserID string, option FindOption) ([]User, error) {
+	respCh := make(chan response, 1)
+	defer close(respCh)
+	req := []interface{}{reqUserID, option}
+	cmd := command{commandFindByID, req, respCh}
+	a.commandCh <- cmd
+	resp := <-respCh
+	var res []User
+	if resp.err != nil {
+		e.Logger.Debugf("User[User=%s] Find Error. [%s]", reqUserID, resp.err)
+		return res, resp.err
+	}
+	if res, ok := resp.result[0].([]User); ok {
+		return res, nil
+	}
+	e.Logger.Debugf("User[User=%s] Find Error. [%s]", reqUserID, ErrorOther)
+	return res, ErrorOther
+}
