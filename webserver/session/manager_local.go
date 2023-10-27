fff72d8919ac42a35e3df0ecfa507835933b8977
CoffeeKiller <18320210361@163.com>
2023-10-27 22:48:51 +0800

    [feat] 完成session管理模块

--- webserver/session/manager_local.go
+++ webserver/session/manager_local.go
@@ -16,8 +16,6 @@ const (
 	commandDeleteExpired                    // 数据有效时间
 )
 
-var e *echo.Echo
-
 // 命令类型指定
 type commandType int
 
@@ -46,7 +44,7 @@ type response struct {
 // mainLoop sessionManager主循环处理
 func (m *Manager) mainLoop() {
 
-	session := make(map[ID]session)
+	sessions := make(map[ID]session)
 	m.stopCh = make(chan struct{}, 1)
 	m.commandCh = make(chan command, 1)
 	defer close(m.commandCh)
@@ -59,19 +57,112 @@ loop:
 			switch cmd.cmdType {
 			// 创建会话数据
 			case commandCreate:
-
+				sessionID := ID(createSessionID())
+				session := session{}
+				sessionStore := Store{}
+				sessionData := make(map[string]string)
+				sessionStore.Data = sessionData
+				sessionStore.ConsistencyToken = createToken()
+				session.store = sessionStore
+				session.expire = time.Now().Add(sessionExpire)
+				sessions[sessionID] = session
+				res := []interface{}{sessionID}
+				e.Logger.Debugf("Session[%s] Create. expire[%s]", sessionID, session.expire)
+				cmd.responseCh <- response{res, nil}
 			// 读取会话数据
 			case commandLoadStore:
-
+				reqSessionID, ok := cmd.req[0].(ID)
+				if !ok {
+					cmd.responseCh <- response{nil, ErrorBadParameter}
+					break
+				}
+				session, ok := sessions[reqSessionID]
+				if !ok {
+					cmd.responseCh <- response{nil, ErrorNotFound}
+					break
+				}
+				if time.Now().After(session.expire) {
+					cmd.responseCh <- response{nil, ErrorNotFound}
+					break
+				}
+				sessionStore := Store{}
+				sessionData := make(map[string]string)
+				for k, v := range session.store.Data {
+					sessionData[k] = v
+				}
+				sessionStore.Data = sessionData
+				sessionStore.ConsistencyToken = session.store.ConsistencyToken
+				session.expire = time.Now().Add(sessionExpire)
+				sessions[reqSessionID] = session
+				e.Logger.Debugf("Session[%s] Load store. store[%s] expire[%s]", reqSessionID, session.store, session.expire)
+				res := []interface{}{sessionStore}
+				cmd.responseCh <- response{res, nil}
 			// 保存会话数据
 			case commandSavaStore:
-
+				reqSessionID, ok := cmd.req[0].(ID)
+				if !ok {
+					cmd.responseCh <- response{nil, ErrorBadParameter}
+					break
+				}
+				reqSessionStore, ok := cmd.req[1].(Store)
+				if !ok {
+					cmd.responseCh <- response{nil, ErrorBadParameter}
+					break
+				}
+				session, ok := sessions[reqSessionID]
+				if !ok {
+					cmd.responseCh <- response{nil, ErrorNotFound}
+					break
+				}
+				if time.Now().After(session.expire) {
+					cmd.responseCh <- response{nil, ErrorNotFound}
+					break
+				}
+				if session.store.ConsistencyToken != reqSessionStore.ConsistencyToken {
+					cmd.responseCh <- response{nil, ErrorInvalidToken}
+					break
+				}
+				sessionStore := Store{}
+				sessionData := make(map[string]string)
+				for k, v := range reqSessionStore.Data {
+					sessionData[k] = v
+				}
+				sessionStore.Data = sessionData
+				sessionStore.ConsistencyToken = createToken()
+				session.store = sessionStore
+				session.expire = time.Now().Add(sessionExpire)
+				sessions[reqSessionID] = session
+				e.Logger.Debugf("Session[%s] Save store. store[%s] expire[%s]", reqSessionID, session.store, session.expire)
+				cmd.responseCh <- response{nil, nil}
 			// 删除会话数据
 			case commandDelete:
-
+				reqSessionID, ok := cmd.req[0].(ID)
+				if !ok {
+					cmd.responseCh <- response{nil, ErrorBadParameter}
+					break
+				}
+				session, ok := sessions[reqSessionID]
+				if !ok {
+					cmd.responseCh <- response{nil, ErrorNotFound}
+					break
+				}
+				if time.Now().After(session.expire) {
+					cmd.responseCh <- response{nil, ErrorNotFound}
+					break
+				}
+				delete(sessions, reqSessionID)
+				e.Logger.Debugf("Session[%s] Delete.", reqSessionID)
+				cmd.responseCh <- response{nil, nil}
 			// 删除到期的会话数据
 			case commandDeleteExpired:
-
+				e.Logger.Debugf("Run Session GC. Now[%s]", time.Now())
+				for k, v := range sessions {
+					if time.Now().After(v.expire) {
+						e.Logger.Debugf("Session[%s] expire delete. expire[%s]", k, v.expire)
+						delete(sessions, k)
+					}
+				}
+				cmd.responseCh <- response{nil, nil}
 			// 默认行为
 			default:
 				cmd.responseCh <- response{nil, ErrorInvalidCommand}
