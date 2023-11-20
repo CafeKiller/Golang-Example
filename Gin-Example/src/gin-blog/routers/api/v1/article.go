3836f7b3dc27e8e6d76128d5ce9b0e51ce72bc47
CoffeeKiller <18320210361@163.com>
2023-11-20 23:19:36 +0800

    [fate] Add routes and add Article ORM_Models

---
+++ Gin-Example/src/gin-blog/routers/api/v1/article.go
@@ -0,0 +1,28 @@
+package v1
+
+import "github.com/gin-gonic/gin"
+
+// GetArticle 获取单个文章
+func GetArticle(cont *gin.Context) {
+
+}
+
+// GetArticles 获取多个文章
+func GetArticles(cont *gin.Context) {
+
+}
+
+// AddArticle 新增文章
+func AddArticle(cont *gin.Context) {
+
+}
+
+// UpdateArticle 修改文章
+func UpdateArticle(cont *gin.Context) {
+
+}
+
+// DeleteArticle 删除文章
+func DeleteArticle(cont *gin.Context) {
+
+}
