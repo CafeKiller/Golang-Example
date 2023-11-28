5b0337d2f88afeac3ae9406b695d29718da9dd31
CoffeeKiller <18320210361@163.com>
2023-11-28 21:55:10 +0800

    [style] 添加swagger文档管理

--- Gin-Example/src/gin-blog/routers/api/v1/tag.go
+++ Gin-Example/src/gin-blog/routers/api/v1/tag.go
@@ -44,7 +44,13 @@ func GetTags(cont *gin.Context) {
 	})
 }
 
-// AddTag 添加一个文章标签
+// @Summary 新增文章标签
+// @Produce  json
+// @Param name query string true "Name"
+// @Param state query int false "State"
+// @Param created_by query int false "CreatedBy"
+// @Success 200 {string} json "{"code":200,"data":{},"msg":"ok"}"
+// @Router /api/v1/tags [post]
 func AddTag(cont *gin.Context) {
 	name := cont.Query("name")
 	state := com.StrTo(cont.DefaultQuery("state", "0")).MustInt()
@@ -80,7 +86,14 @@ func AddTag(cont *gin.Context) {
 
 }
 
-// EditTag 修改文章标签
+// @Summary 修改文章标签
+// @Produce  json
+// @Param id param int true "ID"
+// @Param name query string true "ID"
+// @Param state query int false "State"
+// @Param modified_by query string true "ModifiedBy"
+// @Success 200 {string} json "{"code":200,"data":{},"msg":"ok"}"
+// @Router /api/v1/tags/{id} [put]
 func EditTag(cont *gin.Context) {
 	id := com.StrTo(cont.Param("id")).MustInt()
 	name := cont.Query("name")
